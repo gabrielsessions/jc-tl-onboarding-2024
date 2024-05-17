ca471a6bc24b1afaa5e84bdc936cedf2df0b7b85
Gabriel Sessions <gsess7732@gmail.com>
2024-05-17 01:38:18 -0400

    Initial Hello Message

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "jumbocode-green": "#64dcba",
+        "jumbocode-black": "#2b2b2b"
+      }
+    },
+  },
+  plugins: [],
+}
